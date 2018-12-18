ebed21eafcf2d4884df243704ad2e7b9ed0b4994
Alexander Borshak <borshak@gmail.com>
2018-12-19 00:22:41 +0200

    exersises added

---
+++ client/js/driller/driller.js
@@ -0,0 +1,99 @@
+const driller = function(containerToAdd) {
+
+  const HIDDEN_CLASS = 'hidden';
+  const EXERSISE_CONTAINER_CLASS = 'exersise-container';
+
+  let rootDomElement;
+  let successDrillCounter;
+
+  const layout = function() {
+    return `
+    <h1>DRILLER</h1>
+    <div class="${EXERSISE_CONTAINER_CLASS}"></div>
+    <p><button class="close">Close</button></p>
+    `; 
+  };
+
+  const create = function() {
+    rootDomElement = document.createElement('div');
+    rootDomElement.className = `driller ${HIDDEN_CLASS}`;
+    rootDomElement.innerHTML = layout();
+    containerToAdd.appendChild(rootDomElement);
+    addCloseButtonEventListener();
+  };
+
+  const runExersise = function(langEntity, exersiseContainer, exersises, currentExersise) {
+    const isExersiseForbidden = function() {
+      const result = exersises.length && currentExersise > exersises.length - 1;
+      return result;
+    };
+
+    const isNextExesiseExist = function() {
+      return currentExersise < exersises.length - 1; 
+    };
+
+    console.log(`runExersise ${exersises} - ${currentExersise}`);
+    if (isExersiseForbidden()) {
+      hide();
+      return false;
+    }
+    
+    exersises[currentExersise].create(langEntity, exersiseContainer);
+    exersises[currentExersise].run()
+    .then(result => {
+      if (result) {
+        successDrillCounter += 1;
+        if (successDrillCounter === exersises.length) {
+          console.log(`SUCCESS! Word ${langEntity.phrase} drilled!`);
+        }
+      }
+
+      if (isNextExesiseExist()) {
+        const nextExersise = currentExersise + 1;
+        runExersise(langEntity, exersiseContainer, exersises, nextExersise);
+      } else {
+        hide();
+      }
+    });
+  };
+
+  const show = function(langEntity) {
+    const exersiseContainer = rootDomElement.getElementsByClassName(EXERSISE_CONTAINER_CLASS)[0];
+    const exersises = [exersise_1, exersise_2, exersise_3];
+    rootDomElement.classList.remove(HIDDEN_CLASS);
+
+    successDrillCounter = 0;
+    runExersise(langEntity, exersiseContainer, exersises, 0);    
+  };
+
+  const hide = function() {
+    rootDomElement.classList.add(HIDDEN_CLASS);
+  };
+
+  const addCloseButtonEventListener = function() {
+    rootDomElement.addEventListener('click',
+      function(event) {
+        if (event.target.classList.contains('close')) {
+          hide();
+        }
+      });
+  };
+
+  const removeCloseButtonEventListener = function() {
+    rootDomElement.removeEventListener('click');
+  };
+
+  const destroy = function() {
+    hide();
+    removeCloseButtonEventListener();
+    rootDomElement.remove();
+    rootDomElement = null;
+  };
+
+  return {
+    create,
+    show,
+    hide,
+    destroy
+  };
+};
---
+++ client/js/driller/exersises/exersise-2.js
@@ -0,0 +1,65 @@
+const exersise_2 = (function() {
+
+  const HIDDEN_CLASS = 'hidden';
+
+  // let containerToAdd;
+  let rootDomElement;
+  let resolveFn;
+  
+  const layout = function(langEntity) {
+    return `
+    <h1>Exersise 2</h1>
+    <h3>${langEntity.phrase}</h3>
+    <h3>${langEntity.translation}</h3>
+    <p><button class="success">Success</button> || <button class="cancel">Cancel</button></p>
+    `; 
+  };
+
+  const create = function(langEntity, containerToAdd) {
+    if (rootDomElement) destroy();
+
+    rootDomElement = document.createElement('div');
+    rootDomElement.className = `drill-exersise ${HIDDEN_CLASS}`;
+    rootDomElement.innerHTML = layout(langEntity);
+    containerToAdd.appendChild(rootDomElement);
+  };
+
+  const run = function() {
+    rootDomElement.classList.remove(HIDDEN_CLASS);
+    return new Promise(function(resolve, reject) {
+      resolveFn = resolve;
+      addEventListeners(resolve);
+    });
+
+  };
+
+  const clickEventHandler = function(event) {
+    if (event.target.classList.contains('success')) {
+      destroy();
+      resolveFn(true);
+    } else if (event.target.classList.contains('cancel')) {
+      destroy();
+      resolveFn(false);
+    }
+  };
+
+  const addEventListeners = function() {
+    rootDomElement.addEventListener('click', clickEventHandler);
+  };
+
+  const removeEventListeners = function() {
+    rootDomElement.removeEventListener('click', clickEventHandler);
+  };
+
+  const destroy = function() {
+    removeEventListeners();
+    rootDomElement.remove();
+    rootDomElement = null;
+  };
+  
+  return {
+    create,
+    run,
+    destroy
+  };
+})();
