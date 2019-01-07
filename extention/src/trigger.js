61380fd8b7cf0f5ad4624c020870849804d2e29d
Alexander Borshak <borshak@gmail.com>
2019-01-07 21:55:35 +0200

    logic of extention changed

---
+++ extention/src/trigger.js
@@ -0,0 +1,15 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.insertCSS({
+    file: 'content.css'
+  });
+
+  chrome.tabs.executeScript({
+    file: 'dict.js'
+  });
+
+  chrome.tabs.executeScript({
+    file: 'content.js'
+  });
+
+  chrome.browserAction.setIcon({path:"icon-enabled.png"});
+});
