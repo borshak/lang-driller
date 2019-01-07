8a2ac72693e0e1bfa78ff09939bf5e094ca9397e
Alexander Borshak <borshak@gmail.com>
2019-01-07 19:46:02 +0200

    version 0.1 of Chrome extention created

--- extention/src/content.js
+++ extention/src/content.js
@@ -1,108 +1,14 @@
 // Language module
 const languageModule = (function() {
 
-  const DICTIONARY = {
-    "be": {
-      "exm": {
-        "src": "I think, therefore I am.",
-        "tgt": "Я мыслю, следовательно, существую."
-      },
-      "imp": 2,
-      "phr": "be",
-      "trn": "быть; быть живым, жить; существовать"
-    },
-    "company": {
-      "exm": {
-        "src": "to keep bad company",
-        "tgt": "встречаться, водиться с плохими людьми, вращаться в дурной компании"
-      },
-      "imp": 10,
-      "phr": "company",
-      "trn": "общество, компания"
-    },
-    "henceforth": {
-      "imp": 14,
-      "phr": "henceforth",
-      "trn": "с этого времени, впредь"
-    },
-    "henceforward": {
-      "imp": 15,
-      "phr": "henceforward",
-      "trn": "впредь, отныне"
-    },
-    "learn": {
-      "exm": {
-        "src": "to learn French",
-        "tgt": "учить французский язык"
-      },
-      "imp": 6,
-      "phr": "learn",
-      "trn": "учиться; учить (что-л.); научиться (чему-л.)"
-    },
-    "running": {
-      "exm": {
-        "src": "to make good one's running",
-        "tgt": "не отставать; преуспевать"
-      },
-      "imp": 8,
-      "phr": "running",
-      "trn": "беганье, беготня"
-    },
-    "she": {
-      "exm": {
-        "src": "She was my mother, after all.",
-        "tgt": "В конце концов, она была моей матерью."
-      },
-      "imp": 3,
-      "phr": "she",
-      "trn": "она"
-    },
-    "spelling": {
-      "exm": {
-        "src": "alternative / variant spelling of a word",
-        "tgt": "вариант написания слова"
-      },
-      "imp": 12,
-      "phr": "spelling",
-      "trn": "орфография, правописание"
-    },
-    "that": {
-      "exm": {
-        "src": "that woman",
-        "tgt": "та женщина"
-      },
-      "imp": 5,
-      "phr": "that",
-      "trn": "тот, та, то (иногда этот, эта, этот)"
-    },
-    "the": {
-      "exm": {
-        "src": "the door of the room",
-        "tgt": "дверь (этой) комнаты"
-      },
-      "imp": 1,
-      "phr": "the",
-      "trn": "(определённый артикль)"
-    },
-    "word": {
-      "exm": {
-        "src": "archaic / obsolete words",
-        "tgt": "устаревшие слова"
-      },
-      "imp": 4,
-      "phr": "word",
-      "trn": "слово"
-    },
-    "would": {
-      "exm": {
-        "src": "He told us he would come.",
-        "tgt": "Он сказал нам, что придёт."
-      },
-      "imp": 7,
-      "phr": "would",
-      "trn": "вспомогательный глагол; используется для образования будущего в прошедшем во 2 и 3 лице"
-    }
+  const isPlural = function(phrase) {
+    return phrase.endsWith('s') || phrase.endsWith('\'s');
+  };
 
+  const removePlural = function(phrase) {
+    if (phrase.endsWith('\'s')) return phrase.substring(0, phrase.length - 2);
+    else if (phrase.endsWith('s')) return phrase.substring(0, phrase.length - 1);
+    else return phrase;
   };
 
   const getLangEntity = function(rawPhrase) {
@@ -111,6 +17,8 @@ const languageModule = (function() {
     return new Promise(function(resolve, reject) {
       if (DICTIONARY[phrase]) {
         resolve(DICTIONARY[phrase]);
+      } else if (isPlural(phrase) && DICTIONARY[removePlural(phrase)]) {
+        resolve(DICTIONARY[removePlural(phrase)]);
       } else {
         resolve(null);
       }
@@ -133,10 +41,9 @@ const viewModule = (function() {
   document.body.appendChild(tooltipContainer);
 
   // Add bubble to DOM on init
-  const bubbleDOM = document.createElement('div');
-  bubbleDOM.setAttribute('class', 'lang-driller-selection-bubble');
-  document.body.appendChild(bubbleDOM);
-
+  const bubbleContainer = document.createElement('div');
+  bubbleContainer.setAttribute('class', 'lang-driller-selection-bubble');
+  document.body.appendChild(bubbleContainer);
 
   const showTooltip = function(langEntity) {
     const phrase = `<p class="phrase">${langEntity.phr}</p>`;
@@ -147,31 +54,81 @@ const viewModule = (function() {
     ${phrase}
     ${translation}
     ${example}
-    <i></i>
+    <i class="tooltip-pointer"></i>
   `;
     tooltipContainer.innerHTML = layout;
     tooltipContainer.classList.remove(hiddenCssClass);
   };
+
+  const alignTooltip = function() {
+    const pointToTop = function() {
+      tooltipContainer.classList.remove('point-to-bottom');
+      tooltipContainer.classList.add('point-to-top');
+    };
+
+    const pointToBottom = function() {
+      tooltipContainer.classList.remove('point-to-top');
+      tooltipContainer.classList.add('point-to-bottom');
+    };
+
+    const OFFSET = 16;
+
+    const bubbleGeometry = bubbleContainer.getBoundingClientRect();
+    const tooltipGeometry = tooltipContainer.getBoundingClientRect();
+
+    let tooltipTopPosition = bubbleGeometry.top + window.pageYOffset - OFFSET - tooltipContainer.offsetHeight;
+
+    if (tooltipTopPosition >= 0) {
+      tooltipContainer.style.top = tooltipTopPosition + 'px';
+      pointToBottom();
+    } else {
+      tooltipTopPosition = bubbleGeometry.top + window.pageYOffset + OFFSET + bubbleContainer.offsetHeight;
+      tooltipContainer.style.top = tooltipTopPosition + 'px';
+      pointToTop();
+    }
+
+    let tooltipLeftPosition = (bubbleGeometry.left + bubbleGeometry.width / 2) - (tooltipContainer.offsetWidth / 2);
+
+    const MIN_X_OFFSET = 6;
+    if (tooltipLeftPosition < MIN_X_OFFSET) {
+      const xDelta = Math.abs(tooltipLeftPosition) + MIN_X_OFFSET;
+      tooltipLeftPosition += xDelta;
+      tooltipContainer.style.left = tooltipLeftPosition + 'px';
+
+      // Align pointer
+      const MIN_LEFT_POSITION = 24;
+      const tooltipPointer = tooltipContainer.getElementsByClassName('tooltip-pointer')[0];
+      const pointerGeometry = tooltipPointer.getBoundingClientRect();
+      let pointerLeftPosition = bubbleGeometry.left + (bubbleGeometry.width / 2) - (pointerGeometry.width / 4); // we divide to 4 but not to 2 due to some weirg margin in css
+      if (pointerLeftPosition < MIN_LEFT_POSITION) pointerLeftPosition = MIN_LEFT_POSITION;
+      tooltipPointer.style.left = pointerLeftPosition + 'px';
+    } else {
+      tooltipContainer.style.left = tooltipLeftPosition + 'px';
+    }
+  };
   
   const showBubble = function(x, y, selectedText, langEntity) {
-    bubbleDOM.innerHTML = selectedText;
-    bubbleDOM.style.top = x + 'px';
-    bubbleDOM.style.left = y + 'px';
-    bubbleDOM.style.visibility = 'visible';
+    bubbleContainer.innerHTML = selectedText;
+    bubbleContainer.style.top = (y + window.pageYOffset) +  'px';
+    bubbleContainer.style.left = x + 'px';
+    bubbleContainer.style.visibility = 'visible';
 
     if (langEntity) {
-      bubbleDOM.classList.remove('missing');
+      bubbleContainer.classList.remove('missing');
     } else {
-      bubbleDOM.classList.add('missing');
+      bubbleContainer.classList.add('missing');
     }
   };
 
   const showTranslation = function(selectionPosition, selectedText, langEntity) {
-    const OFFSET = 15;
+    const OFFSET = 16;
     const {x, y} = selectionPosition;
 
     showBubble(x + OFFSET, y + OFFSET, selectedText, langEntity);
-    if (langEntity) showTooltip(langEntity);
+    if (langEntity) {
+      showTooltip(langEntity);
+      alignTooltip();
+    }
   };
 
   const hideTooltip = function() {
@@ -179,7 +136,7 @@ const viewModule = (function() {
   };
 
   const hideBubble = function() {
-    bubbleDOM.style.visibility = 'hidden';
+    bubbleContainer.style.visibility = 'hidden';
   };
 
   const hideTranslation = function() {
@@ -196,14 +153,6 @@ const viewModule = (function() {
 
 // Controller
 const controller = (function() {
-  // Predicate, returns true if some text selected
-  // const isSomethingSelected = function(selection) {
-  //   return (selection 
-  //     && selection.toString
-  //     && typeof selection.toString === 'function'
-  //     && selection.toString().trim());
-  // };
-
   const getTextOfSelection = function(selection) {
     return selection.toString().trim().toLowerCase();
   };
@@ -211,9 +160,10 @@ const controller = (function() {
   const getPositionOfSelection = function(selection) {
     const oRange = selection.getRangeAt(0);
     const oRect = oRange.getBoundingClientRect();
+
     return {
-      x: oRect.y,
-      y: oRect.x
+      x: oRect.left,
+      y: oRect.top
     };
   };
 
@@ -224,13 +174,9 @@ const controller = (function() {
 
     if (selectedText) {
       const position = getPositionOfSelection(selection);
-      // const oRange = selection.getRangeAt(0); //get the text range
-      // const oRect = oRange.getBoundingClientRect();
-      console.log('position ->', position);
 
       languageModule.getLangEntity(selectedText)
         .then(function(langEntity) {
-        console.log('LE =>', langEntity);      
         viewModule.showTranslation(position, selectedText, langEntity);
       });
     }
